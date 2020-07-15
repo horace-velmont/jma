694c04af90c80a99b422c8a3f8c858d1f9f209e8
Horace Velmont <dragond7@naver.com>
2020-07-15 20:30:49 +0900

    형식 변경

--- src/main/java/worka/holic/app/jandimember/controller/MemberController.java
+++ src/main/java/worka/holic/app/jandimember/controller/MemberController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("member")
+@RequestMapping("/member")
 public class MemberController {
     private Map<String, String> memberMap;
 
@@ -20,9 +20,9 @@ public class MemberController {
         memberMap.put("1컴이", "horace-velmont");
     }
 
-    @GetMapping("/")
-    public ResponseEntity<String> showMemberByName(@RequestParam String memberName) {
-        Optional<String> memberId = Optional.of(memberMap.get("memberName"));
+    @GetMapping("")
+    public ResponseEntity<String> showMemberByName(@RequestParam("member_name") String memberName) {
+        Optional<String> memberId = Optional.of(memberMap.get(memberName));
         return ResponseEntity.of(memberId);
     }
 }
